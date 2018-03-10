e74c2a657b478d7764dcc1809bccd552b313e951
ainaimi <ashley.naimi@pitt.edu>
2018-03-10 13:16:41 -0500

    updated example file

--- R/preliminary_example.R
+++ R/preliminary_example.R
@@ -1,4 +1,6 @@
 
+library(boot)
+
 d<-data.frame(x=c(rep(0,16),rep(1,16)),
               m=c(rep(0,8),rep(1,8),rep(0,8),rep(1,8)),
               c_xy=rep(c(rep(0,4),rep(1,4)),4),
@@ -24,16 +26,14 @@ exposure<-D$x
 famX=binomial
 famM=binomial
 
-dat=D
-
 cde<-function(outcome,mY,famY,
               mediator,mM,famM,
               exposure,mX,famX,
               data,interactionRR=T,interactionRD=T){
 
   # IPW
-  X_propensity<-glm(mX,data=dat,family=famX)$fitted.values
-  M_propensity<-glm(mM,data=dat,family=famM)$fitted.values
+  X_propensity<-glm(mX,data=data,family=famX)$fitted.values
+  M_propensity<-glm(mM,data=data,family=famM)$fitted.values
 
   den<-(X_propensity*exposure + (1-X_propensity)*(1-exposure))*(M_propensity*mediator + (1-M_propensity)*(1-mediator))
   num<-(mean(exposure)*exposure + (1-mean(exposure))*(1-exposure))*(mean(mediator)*mediator + (1-mean(mediator))*(1-mediator))
@@ -41,18 +41,18 @@ cde<-function(outcome,mY,famY,
   sw=num/den
 
   if(interactionRD==T){
-    RD<-coef(glm(outcome~exposure+mediator+exposure:mediator,data=dat,family=gaussian(link="identity"),weights=sw))["exposure"]
+    RD<-coef(glm(outcome~exposure+mediator+exposure:mediator,data=data,family=gaussian(link="identity"),weights=sw))["exposure"]
     names(RD)<-"risk difference"
   } else{
-    RD<-coef(glm(outcome~exposure+mediator,data=dat,family=gaussian(link="identity"),weights=sw))["exposure"]
+    RD<-coef(glm(outcome~exposure+mediator,data=data,family=gaussian(link="identity"),weights=sw))["exposure"]
     names(RD)<-"risk difference"
   }
 
   if(interactionRR==T){
-    RR<-exp(coef(glm(outcome~exposure+mediator+exposure:mediator,data=dat,family=poisson(link="log"),weights=sw))["exposure"])
+    RR<-exp(coef(glm(outcome~exposure+mediator+exposure:mediator,data=data,family=poisson(link="log"),weights=sw))["exposure"])
     names(RR)<-"risk ratio"
   } else{
-    RR<-exp(coef(glm(outcome~exposure+mediator,data=dat,family=poisson(link="log"),weights=sw))["exposure"])
+    RR<-exp(coef(glm(outcome~exposure+mediator,data=data,family=poisson(link="log"),weights=sw))["exposure"])
     names(RR)<-"risk ratio"
   }
 
@@ -60,23 +60,23 @@ cde<-function(outcome,mY,famY,
 
   # structural transformation
   if(interactionRD==T){
-    mod1<-matrix(coef(lm(y~x+m+x:m+c_xy+c_my,data=dat))[c("m","x:m")])
+    mod1<-matrix(coef(lm(y~x+m+x:m+c_xy+c_my,data=data))[c("m","x:m")])
     y_tilde<-outcome - matrix(c(mediator,mediator*exposure),ncol=2)%*%mod1
-    RD<-coef(lm(y_tilde~exposure+c_xy,data=dat))["exposure"]
+    RD<-coef(lm(y_tilde~exposure+c_xy,data=data))["exposure"]
   } else{
-    mod1<-matrix(coef(lm(y~x+m+c_xy+c_my,data=dat))[c("m")])
+    mod1<-matrix(coef(lm(y~x+m+c_xy+c_my,data=data))[c("m")])
     y_tilde<-outcome - matrix(mediator,ncol=1)%*%mod1
-    RD<-coef(lm(y_tilde~exposure+c_xy,data=dat))["exposure"]
+    RD<-coef(lm(y_tilde~exposure+c_xy,data=data))["exposure"]
   }
 
   if(interactionRR==T){
-    mod1<-matrix(coef(glm(y~x+m+x:m+c_xy+c_my,data=dat,family=poisson))[c("m","x:m")])
+    mod1<-matrix(coef(glm(y~x+m+x:m+c_xy+c_my,data=data,family=poisson))[c("m","x:m")])
     y_tilde<-outcome*exp(- matrix(c(mediator,mediator*exposure),ncol=2)%*%mod1)
-    RR<-exp(coef(glm(y_tilde~exposure+c_xy,data=dat,family=quasipoisson))["exposure"])
+    RR<-exp(coef(glm(y_tilde~exposure+c_xy,data=data,family=quasipoisson))["exposure"])
   } else{
-    mod1<-matrix(coef(glm(y~x+m+c_xy+c_my,data=dat,family=poisson))[c("m")])
+    mod1<-matrix(coef(glm(y~x+m+c_xy+c_my,data=data,family=poisson))[c("m")])
     y_tilde<-outcome*exp( - matrix(mediator,ncol=1)%*%mod1)
-    RR<-exp(coef(glm(y_tilde~exposure+c_xy,data=dat,family=poisson))["exposure"])
+    RR<-exp(coef(glm(y_tilde~exposure+c_xy,data=data,family=poisson))["exposure"])
   }
 
   names(RD)<-"Risk Difference"
@@ -85,23 +85,23 @@ cde<-function(outcome,mY,famY,
 
   # G - estimation
   if(interactionRD==T){
-    mod1<-matrix(coef(lm(y~x+I(m-M_propensity)+x:I(m-M_propensity)+c_my,data=dat))[c("I(m - M_propensity)","x:I(m - M_propensity)")])
+    mod1<-matrix(coef(lm(y~x+I(m-M_propensity)+x:I(m-M_propensity)+c_my,data=data))[c("I(m - M_propensity)","x:I(m - M_propensity)")])
     y_tilde<-outcome - matrix(c(mediator,mediator*exposure),ncol=2)%*%mod1
-    RD<-coef(lm(y_tilde~I(x-X_propensity)+c_xy,data=dat))["I(x - X_propensity)"]
+    RD<-coef(lm(y_tilde~I(x-X_propensity)+c_xy,data=data))["I(x - X_propensity)"]
   } else{
-    mod1<-matrix(coef(lm(y~x+I(m-M_propensity)+c_my,data=dat))[c("I(m - M_propensity)")])
+    mod1<-matrix(coef(lm(y~x+I(m-M_propensity)+c_my,data=data))[c("I(m - M_propensity)")])
     y_tilde<-outcome - matrix(mediator,ncol=1)%*%mod1
-    RD<-coef(lm(y_tilde~I(x-X_propensity)+c_xy,data=dat))["I(x - X_propensity)"]
+    RD<-coef(lm(y_tilde~I(x-X_propensity)+c_xy,data=data))["I(x - X_propensity)"]
   }
 
   if(interactionRR==T){
-    mod1<-matrix(coef(glm(y~x+I(m-M_propensity)+x:I(m-M_propensity)+c_xy+c_my,family=poisson,data=dat))[c("I(m - M_propensity)","x:I(m - M_propensity)")])
+    mod1<-matrix(coef(glm(y~x+I(m-M_propensity)+x:I(m-M_propensity)+c_xy+c_my,family=poisson,data=data))[c("I(m - M_propensity)","x:I(m - M_propensity)")])
     y_tilde<-outcome*exp( - matrix(c(mediator,mediator*exposure),ncol=2)%*%mod1)
-    RR<-exp(coef(glm(y_tilde~I(x-X_propensity)+c_xy,family=quasipoisson,data=dat))["I(x - X_propensity)"])
+    RR<-exp(coef(glm(y_tilde~I(x-X_propensity)+c_xy,family=quasipoisson,data=data))["I(x - X_propensity)"])
   } else{
-    mod1<-matrix(coef(glm(y~x+I(m-M_propensity)+c_xy+c_my,family=poisson,data=dat))[c("I(m - M_propensity)")])
+    mod1<-matrix(coef(glm(y~x+I(m-M_propensity)+c_xy+c_my,family=poisson,data=data))[c("I(m - M_propensity)")])
     y_tilde<-outcome*exp( - matrix(mediator,ncol=1)%*%mod1)
-    RR<-exp(coef(lm(y_tilde~I(x-X_propensity)+c_xy,family=quasipoisson,data=dat))["I(x - X_propensity)"])
+    RR<-exp(coef(lm(y_tilde~I(x-X_propensity)+c_xy,family=quasipoisson,data=data))["I(x - X_propensity)"])
   }
 
   names(RD)<-"Risk Difference"
@@ -110,33 +110,33 @@ cde<-function(outcome,mY,famY,
 
 
   ## TMLE
-  mY<-glm(y~c_xy+c_my,data=dat,subset=x==1&m==0,family=binomial(link="logit"))
-  mM<-glm(m~c_my,data=dat,subset=x==1,family=binomial(link="logit"))
-  mX<-glm(x~c_xy,data=dat,family=binomial(link="logit"))
-  newDat1<-dat;newDat1$x<-1;newDat1$m<-0;
-  pY<-predict(mY,newdata=newDat1,type="response")
-  pM<-predict(mM,newdata=newDat1,type="response")
-  pX<-predict(mX,newdata=newDat1,type="response")
-  dat$cc2 <- as.numeric(dat$x==1&dat$m==0)/((1-pM)*(pX))
-  q2_star<-glm(y~-1+cc2,data=dat,offset=log(pY/(1-pY)),family=binomial(link="logit"))$fitted.values
-  mQ1<-glm(q2_star ~ c_xy,data=dat,subset=x==1,family=quasibinomial(link="logit"))
-  q1<-predict(mQ1,newdata=newDat1,type="response")
-  dat$cc1 <- as.numeric(dat$x==1)/pX
-  mu10<-glm(q2_star ~ -1 + cc1, data=dat,family=quasibinomial(link="logit"),offset=log(q1/(1-q1)))$fitted.values
+  mY<-glm(y~c_xy+c_my,data=data,subset=x==1&m==0,family=binomial(link="logit"))
+  mM<-glm(m~c_my,data=data,subset=x==1,family=binomial(link="logit"))
+  mX<-glm(x~c_xy,data=data,family=binomial(link="logit"))
+  newdata1<-data;newdata1$x<-1;newdata1$m<-0;
+  pY<-predict(mY,newdata=newdata1,type="response")
+  pM<-predict(mM,newdata=newdata1,type="response")
+  pX<-predict(mX,newdata=newdata1,type="response")
+  data$cc2 <- as.numeric(data$x==1&data$m==0)/((1-pM)*(pX))
+  q2_star<-glm(y~-1+cc2,data=data,offset=log(pY/(1-pY)),family=binomial(link="logit"))$fitted.values
+  mQ1<-glm(q2_star ~ c_xy,data=data,subset=x==1,family=quasibinomial(link="logit"))
+  q1<-predict(mQ1,newdata=newdata1,type="response")
+  data$cc1 <- as.numeric(data$x==1)/pX
+  mu10<-glm(q2_star ~ -1 + cc1, data=data,family=quasibinomial(link="logit"),offset=log(q1/(1-q1)))$fitted.values
 
-  mY<-glm(y~c_xy+c_my,data=dat,subset=x==0&m==0,family=binomial(link="logit"))
-  mM<-glm(m~c_my,data=dat,subset=x==0,family=binomial(link="logit"))
-  mX<-glm(x~c_xy,data=dat,family=binomial(link="logit"))
-  newDat1<-dat;newDat1$x<-0;newDat1$m<-0;
-  pY<-predict(mY,newdata=newDat1,type="response")
-  pM<-predict(mM,newdata=newDat1,type="response")
-  pX<-predict(mX,newdata=newDat1,type="response")
-  dat$cc2 <- as.numeric(dat$x==0&dat$m==0)/((1-pM)*(pX))
-  q2_star<-glm(y~-1+cc2,data=dat,offset=log(pY/(1-pY)),family=binomial(link="logit"))$fitted.values
-  mQ1<-glm(q2_star ~ c_xy,data=dat,subset=x==0,family=quasibinomial(link="logit"))
-  q1<-predict(mQ1,newdata=newDat1,type="response")
-  dat$cc1 <- as.numeric(dat$x==0)/pX
-  mu00<-glm(q2_star ~ -1 + cc1, data=dat,family=quasibinomial(link="logit"),offset=log(q1/(1-q1)))$fitted.values
+  mY<-glm(y~c_xy+c_my,data=data,subset=x==0&m==0,family=binomial(link="logit"))
+  mM<-glm(m~c_my,data=data,subset=x==0,family=binomial(link="logit"))
+  mX<-glm(x~c_xy,data=data,family=binomial(link="logit"))
+  newdata1<-data;newdata1$x<-0;newdata1$m<-0;
+  pY<-predict(mY,newdata=newdata1,type="response")
+  pM<-predict(mM,newdata=newdata1,type="response")
+  pX<-predict(mX,newdata=newdata1,type="response")
+  data$cc2 <- as.numeric(data$x==0&data$m==0)/((1-pM)*(pX))
+  q2_star<-glm(y~-1+cc2,data=data,offset=log(pY/(1-pY)),family=binomial(link="logit"))$fitted.values
+  mQ1<-glm(q2_star ~ c_xy,data=data,subset=x==0,family=quasibinomial(link="logit"))
+  q1<-predict(mQ1,newdata=newdata1,type="response")
+  data$cc1 <- as.numeric(data$x==0)/pX
+  mu00<-glm(q2_star ~ -1 + cc1, data=data,family=quasibinomial(link="logit"),offset=log(q1/(1-q1)))$fitted.values
 
   RD<-mean(mu10-mu00)
   RR<-mean(mu10)/mean(mu00)
@@ -152,4 +152,4 @@ cde<-function(outcome,mY,famY,
 cde(outcome=D$y,mY=as.formula(y~x+m+x:m+c_xy+c_my),binomial,
     mediator=D$m,mM=as.formula(m~x+c_xy+c_my),binomial,
     exposure=D$x,mX=as.formula(x~c_xy),binomial,
-    dat=D,interactionRD=T,interactionRR=T)
+    data=D,interactionRD=T,interactionRR=T)
